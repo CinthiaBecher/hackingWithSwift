c25ff77a3f080418a33b47666e663c06dc3bcd1c
CinthiaBecher <cinthia_becher@hotmail.com>
2023-07-19 20:50:24 -0300

    checkpoint 02

---
+++ checkpoint-02.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+let albums = ["Taylor Swift",
+              "Fearless",
+              "Speak Now",
+              "Red",
+              "1989",
+              "Reputation",
+              "Lover",
+              "Folklore",
+              "Evermore",
+              "Taylor Swift",
+              "Fearless",
+              "Speak Now",
+              "Red",
+              "1989",
+              "Reputation",
+              "Lover",
+              "Folklore",
+              "Evermore"]
+
+print(albums.count)
+
+let albumsSet = Set(albums)
+print(albumsSet.count)
+

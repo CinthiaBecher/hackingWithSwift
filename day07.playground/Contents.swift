73febc8dded7a041d583e82606ea28f08b20de17
CinthiaBecher <cinthia_becher@hotmail.com>
2024-10-16 19:12:25 -0300

    day 07

--- day07.playground/Contents.swift
+++ day07.playground/Contents.swift
@@ -1,3 +1,48 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func printTimesTables(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5, end: 20)
+
+printTimesTables(number: 2, end: 10)
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+func pythagoras(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+
+
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+let user = getUser()
+let firstName1 = user.firstName
+
+let (firstName2, _) = getUser()
+print("Name: \(firstName2)")
+let lastName = user.lastName
+
+print("Name: \(firstName2) \(lastName)")
+
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let result2 = isUppercase(string)

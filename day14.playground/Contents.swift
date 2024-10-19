62e76db39f9651edb79c948042703fbbb35c1884
CinthiaBecher <cinthia_becher@hotmail.com>
2024-10-19 20:06:48 -0300

    day 14

--- day14.playground/Contents.swift
+++ day14.playground/Contents.swift
@@ -1,3 +1,76 @@
 import UIKit
 
-var greeting = "Hello, playground"
+///Optionals
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+
+var username: String? = nil
+username = "cinthia"
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+var number: Int? = nil
+//print(square(number: number)) -----> ERROR
+if let unwrappedNumber = number {
+    print(square(number: unwrappedNumber))
+}
+
+///How to unwrap optionals with guard
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+
+    print("\(number) x \(number) is \(number * number)")
+}
+
+printSquare(of: 5)
+
+///How to unwrap optionals with nil coalescing
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+//let new = captains["Serenity"] ----->NOTHING
+let new = captains["Serenity"] ?? "N/A"
+let new2 = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+print(favorite)
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+
+///**Checkpoint 9**
+///write a function that accepts an optional array of integers, and returns one randomly. If the array is missing or empty, return a random number in the range 1 through 100.
+///If that sounds easy, it’s because I haven’t explained the catch yet: I want you to write your function in a single line of code.
+
+func randomOne(for array: [Int]?) -> Int {return array?.randomElement() ?? 0}
+print(randomOne(for:[1,2,3]))
+print(randomOne(for:nil))

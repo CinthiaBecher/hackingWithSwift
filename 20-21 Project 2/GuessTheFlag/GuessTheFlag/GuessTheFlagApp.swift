4833e3b946ba6ff27203d5abb45e192f80a1b3f1
CinthiaBecher <cinthia_becher@hotmail.com>
2024-10-30 20:42:57 -0300

    day 21

---
+++ 20-21 Project 2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Cinthia Becher on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
